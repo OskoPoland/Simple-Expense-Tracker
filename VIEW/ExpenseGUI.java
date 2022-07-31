4aecd801c7a767bd42073059cf7603db429adfd2
OskoPoland <106214946+OskoPoland@users.noreply.github.com>
2022-07-30 22:24:54 -0700

    Create ExpenseGUI.java
    just starting out this is all a placeholder here

---
+++ VIEW/ExpenseGUI.java
@@ -0,0 +1,14 @@
+package VIEW;
+import CONTROLLER.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ExpenseGUI {
+    JFrame frame = new JFrame();
+    DataControls dc = new DataControls();
+
+    public void expenseConstruct() {
+
+    }
+}
